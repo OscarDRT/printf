9f5118c87e538c2cbd3c6262b2315d20fec2d406
Camilo Morales <camiloandres.1509@gmail.com>
2019-10-26 16:29:31 -0500

    Updating

--- principal.c
+++ principal.c
@@ -4,17 +4,18 @@
 int _printf(const char *format, ...)
 {
 	formaton ops[] = {
-	    {"%", op_char},
-	     {"c", op_char},
-	    {"s", op_string},
-	    {"d", op_int_b10},
-	    {"i", op_integer},
-	    {NULL, NULL}};
+	{'%', op_mod},
+	{"c", op_char},
+	{"s", op_string},
+	{"d", op_int_b10},
+	{"i", op_integer},
+	{NULL, NULL}};
+	
 	int i = 0;
 	const char *recorrer;
 	
-	va_list valist;
-	va_start(valist, format);
+	va_list o;
+	va_start(o, format);
 	for (recorrer = format; *recorrer != '\0'; recorrer++)
 	{
 		while (*recorrer != '%')
@@ -22,12 +23,12 @@ int _printf(const char *format, ...)
 			_putchar(*recorrer);
 			recorrer++;
 		}
-		recorrer++
+		recorrer++;
 		while (recorrer != NULL)
 		{
 			if (*recorrer == *(ops[i].formato))
 			{
-				ops[i].f(va_list valist);
+				ops[i].f(o);
 			}
 			i++;
 		}
